687fd7afe3b798ffc2a1dd52b74cc85c0f6a17f0
Raque alexy L. Paradillo <raparadillo06235@liceo.edu.ph>
2025-08-01 11:55:20 +0800

    fixed to meet criteria

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,36 @@
 import java.util.Scanner;
 
 public class Main {
-    static int number = 0;
-    static int count =1;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        while (count<=5) {
-            System.out.print("Enter number " + count+ ": " );
+        int num1 = 0, num2 = 0, num3 = 0, num4 = 0, num5 = 0;
 
-            number = number+scanner.nextInt(); // asks for a number and adds it on the "number" variable
-            count++;
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i + ": ");
+            int input = scanner.nextInt();
 
 
+            switch (i) {
+                case 1:
+                    num1 = input;
+                    break;
+                case 2:
+                    num2 = input;
+                    break;
+                case 3:
+                    num3 = input;
+                    break;
+                case 4:
+                    num4 = input;
+                    break;
+                case 5:
+                    num5 = input;
+                    break;
+            }
         }
-        System.out.println("Total Number: "+number);
+
+        int total = num1 + num2 + num3 + num4 + num5;
+        System.out.println("Total Number: " + total);
     }
 }
