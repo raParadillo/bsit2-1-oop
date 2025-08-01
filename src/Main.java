f8078b7ff85830625b5ab5466c257e33d6bc4ae1
Raque alexy L. Paradillo <raparadillo06235@liceo.edu.ph>
2025-08-01 11:18:50 +0800

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
 
 public class Main {
+    static int number = 0;
+    static int count =1;
     public static void main(String[] args) {
-        System.out.println("Main branch!");
+
+        Scanner scanner = new Scanner(System.in);
+        while (count<=5) {
+            System.out.print("Enter number " + count+ ": " );
+            try{
+                number = number+scanner.nextInt();
+                count++;
+
+            }catch (Exception e) {
+                System.out.println("Invalid input. Please enter an integer.");
+                scanner.nextLine();
+            }
+
+
+        }
+        System.out.println("Total Number: "+number);
     }
 }
