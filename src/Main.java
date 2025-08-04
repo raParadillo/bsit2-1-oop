2f843543438c95512efbecaa91873184ed0b8327
Raque alexy L. Paradillo <raparadillo06235@liceo.edu.ph>
2025-08-04 10:02:31 +0800

    Library System

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main branch!");
+        Book book1 = new Book("Dustborn", "Erin Bowman", 432);
+        Book book2 = new Book("A Time to Kill", "Harper Lee", 281);
+        Book book3 = new Book("The Great Gatsby", "John Grisham", 672);
+
     }
+
+
 }
